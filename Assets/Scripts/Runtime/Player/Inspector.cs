db88c954901029f041c000278d16eff63327d76d
smokeythekat <owr5273@gmail.com>
2024-08-26 23:04:42 -0300

    yo

--- Assets/Scripts/Runtime/Player/Inspector.cs
+++ Assets/Scripts/Runtime/Player/Inspector.cs
@@ -7,117 +7,138 @@ using UnityEngine.InputSystem;
 
 namespace PsychoSerum.Player
 {
-    [RequireComponent(typeof(PlayerInput))]
-    internal class Inspector : MonoBehaviour
-    {
-        [Header("References")]
-        [SerializeField] private PlayerInput _playerInput;
-        [SerializeField] private GameObject _offset;
-        [SerializeField] private PlayerController _controller;
-        [SerializeField] private GameObject _head;
+	[RequireComponent(typeof(PlayerInput))]
+	internal class Inspector : MonoBehaviour
+	{
+		[Header("References")]
+		[SerializeField] private PlayerInput _playerInput;
+		[SerializeField] private GameObject _offset;
+		[SerializeField] private PlayerController _controller;
+		[SerializeField] private GameObject _head;
 
-        private Vector3 _lastMousePosition;
-        private Transform _examinedObject;
-        private ExamineType _type;
-        private GameObject _objectOffset;
+		private Vector3 _lastMousePosition;
+		private Transform _examinedObject;
+		private ExamineType _type;
+		private GameObject _objectOffset;
 
-        private Vector3 _origHeadPosition;
-        private Quaternion _origHeadRotation;
+		private Vector3 _origHeadPosition;
+		private Quaternion _origHeadRotation;
 
-        private Dictionary<Transform, Vector3> _origPositions = new Dictionary<Transform, Vector3>();
-        private Dictionary<Transform, Quaternion> _origRotations = new Dictionary<Transform, Quaternion>();
+		private Dictionary<Transform, Vector3> _origPositions = new Dictionary<Transform, Vector3>();
+		private Dictionary<Transform, Quaternion> _origRotations = new Dictionary<Transform, Quaternion>();
 
-        private bool _isExaming;
+		private bool _isExaming;
+		private bool _movingBack = false;
 
-        public void StartExamine(Transform obj, ExamineType type, GameObject offset)
-        {
-            if (obj == null) return;
+		public void StartExamine(Transform obj, ExamineType type, GameObject offset)
+		{
+			if (obj == null) return;
 
-            _isExaming = true;
+			_isExaming = true;
 
-            _examinedObject = obj;
-            _type = type;
-            _objectOffset = offset;
+			_examinedObject = obj;
+			_type = type;
+			_objectOffset = offset;
 
-            _origHeadPosition = _head.transform.position;
-            _origHeadRotation = _head.transform.rotation;
+			_origHeadPosition = _head.transform.position;
+			_origHeadRotation = _head.transform.rotation;
 
-            _origPositions[_examinedObject] = _examinedObject.position;
-            _origRotations[_examinedObject] = _examinedObject.rotation;
+			_origPositions[_examinedObject] = _examinedObject.position;
+			_origRotations[_examinedObject] = _examinedObject.rotation;
 
-            _lastMousePosition = Input.mousePosition;
-            Cursor.lockState = CursorLockMode.None;
-            Cursor.visible = true;
-            _playerInput.enabled = false;
-            _controller.Disable();
-        }
+			_lastMousePosition = Input.mousePosition;
+			Cursor.lockState = CursorLockMode.None;
+			Cursor.visible = true;
+			_playerInput.enabled = false;
+			_controller.Disable();
 
-        public void EndExamine()
-        {
-            if (_type == ExamineType.ComeTo)
-            {
-                _head.transform.position = _origHeadPosition;
-                _head.transform.rotation = _origHeadRotation;
-            }
+			Rigidbody rb = obj.GetComponent<Rigidbody>();
+			if (rb)
+			{
+				rb.isKinematic = true;
+			}
+		}
 
-            _isExaming = false;
+		public void EndExamine()
+		{
+			if (_type == ExamineType.ComeTo)
+			{
+				_head.transform.position = _origHeadPosition;
+				_head.transform.rotation = _origHeadRotation;
+			}
 
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-            _playerInput.enabled = true;
-            _controller.Enable();
-        }
+			_isExaming = false;
 
-        void Examine()
-        {
-            if (_examinedObject != null)
-            {
+			Cursor.lockState = CursorLockMode.Locked;
+			Cursor.visible = false;
+			_playerInput.enabled = true;
+			_controller.Enable();
 
-                if (_type == ExamineType.Goto)
-                {
-                    _examinedObject.position = Vector3.Lerp(_examinedObject.position, _offset.transform.position, 0.2f);
+			_movingBack = true;
+		}
 
-                    Vector3 deltaMouse = Input.mousePosition - _lastMousePosition;
-                    float rotationSpeed = 1.0f;
-                    _examinedObject.Rotate(deltaMouse.x * rotationSpeed * Vector3.up, Space.World);
-                    _examinedObject.Rotate(deltaMouse.y * rotationSpeed * Vector3.left, Space.World);
-                    _lastMousePosition = Input.mousePosition;
-                }
-                else if (_type == ExamineType.ComeTo)
-                {
-                    _head.transform.rotation = Quaternion.LookRotation(-(_examinedObject.transform.position - _objectOffset.transform.position).normalized);
-                    _head.transform.position = Vector3.Lerp(_head.transform.position, _objectOffset.transform.position, 0.2f);
-                }
-            }
-        }
+		void Examine()
+		{
+			if (_examinedObject != null)
+			{
 
-        private void CancelExamine()
-        {
-            if (_examinedObject == null) return;
+				if (_type == ExamineType.Goto)
+				{
+					_examinedObject.position = Vector3.Lerp(_examinedObject.position, _offset.transform.position, 0.2f);
 
-            if (_origPositions.ContainsKey(_examinedObject))
-            {
-                _examinedObject.position = Vector3.Lerp(_examinedObject.position, _origPositions[_examinedObject], 0.2f);
-            }
+					Vector3 deltaMouse = Input.mousePosition - _lastMousePosition;
+					float rotationSpeed = 1.0f;
+					_examinedObject.Rotate(deltaMouse.x * rotationSpeed * Vector3.up, Space.World);
+					_examinedObject.Rotate(deltaMouse.y * rotationSpeed * Vector3.left, Space.World);
+					_lastMousePosition = Input.mousePosition;
+				}
+				else if (_type == ExamineType.ComeTo)
+				{
+					_head.transform.rotation = Quaternion.LookRotation(-(_examinedObject.transform.position - _objectOffset.transform.position).normalized);
+					_head.transform.position = Vector3.Lerp(_head.transform.position, _objectOffset.transform.position, 0.2f);
+				}
+			}
+		}
 
-            if (_origRotations.ContainsKey(_examinedObject))
-            {
-                _examinedObject.rotation = Quaternion.Slerp(_examinedObject.rotation, _origRotations[_examinedObject], 0.2f);
-            }
-        }
+		private void CancelExamine()
+		{
+			if (_examinedObject == null) return;
 
-        private void Awake()
-        {
-            if (_playerInput == null) _playerInput = GetComponent<PlayerInput>();
-            if (_controller == null) _controller = GetComponent<PlayerController>();
-        }
+			if (_origPositions.ContainsKey(_examinedObject))
+			{
+				_examinedObject.position = Vector3.Lerp(_examinedObject.position, _origPositions[_examinedObject], 0.2f);
+			}
 
-        private void Update()
-        {
-            if (Input.GetKeyDown(KeyCode.Escape)) EndExamine();
-            if (_isExaming) Examine();
-            else CancelExamine();
-        }
-    }
+			if (_origRotations.ContainsKey(_examinedObject))
+			{
+				_examinedObject.rotation = Quaternion.Slerp(_examinedObject.rotation, _origRotations[_examinedObject], 0.2f);
+			}
+
+			if (
+				(_examinedObject.position - _origPositions[_examinedObject]).magnitude < 0.01
+			)
+			{
+				Rigidbody rb = _examinedObject.GetComponent<Rigidbody>();
+				if (rb)
+				{
+					rb.isKinematic = false;
+				}
+				_movingBack = false;
+			}
+		}
+
+		private void Awake()
+		{
+			if (_playerInput == null) _playerInput = GetComponent<PlayerInput>();
+			if (_controller == null) _controller = GetComponent<PlayerController>();
+		}
+
+		private void Update()
+		{
+			if (Input.GetKeyDown(KeyCode.Escape)) EndExamine();
+			if (_isExaming) Examine();
+			else if (_movingBack) CancelExamine();
+		}
+	}
 
 }
--- Assets/Scripts/Runtime/Puzzle/Maze/MazeController.cs
+++ Assets/Scripts/Runtime/Puzzle/Maze/MazeController.cs
@@ -8,219 +8,226 @@ using PsychoSerum.MonoSystem;
 
 namespace PsychoSerum.Puzzle
 {
-    internal class MazeController : PuzzleController
-    {
-        [Header("Maze Settings")]
-        [SerializeField][Min(7)] private int _mazeWidth;
-        [SerializeField][Min(2)] private int _mazeHeight;
+	internal class MazeController : PuzzleController
+	{
+		[Header("Maze Settings")]
+		[SerializeField][Min(7)] private int _mazeWidth;
+		[SerializeField][Min(2)] private int _mazeHeight;
 
-        [Header("Maze Prefabs")]
-        [SerializeField] private GameObject _wallPrefab;
-        [SerializeField] private GameObject _floorPrefab;
-        [SerializeField] private GameObject _keyPrefab;
+		[Header("Maze Prefabs")]
+		[SerializeField] private GameObject _wallPrefab;
+		[SerializeField] private GameObject _floorPrefab;
+		[SerializeField] private GameObject _keyPrefab;
 
-        [Header("Gate Settings")]
-        [SerializeField] private Transform _gate;
-        [SerializeField] private Vector2 _gateLimits;
-        [SerializeField] private AudioSource _gateAudioSrc;
-        [SerializeField] private AudioClip _gateAudioClip;
+		[Header("Gate Settings")]
+		[SerializeField] private Transform _gate;
+		[SerializeField] private Vector2 _gateLimits;
+		[SerializeField] private AudioSource _gateAudioSrc;
+		[SerializeField] private AudioClip _gateAudioClip;
 
-        private List<GameObject> _objects;
+		private List<GameObject> _objects;
 
-        private float _scaleX, _scaleZ;
+		private float _scaleX, _scaleZ;
 
-        private Maze _maze;
-        private GameObject _mazeHolder;
+		private Maze _maze;
+		private GameObject _mazeHolder;
 
-        private bool _isStarted = false;
-        private bool _isTimerRunning = false;
+		private bool _isStarted = false;
+		private bool _isTimerRunning = false;
 
-        [SerializeField] private float _timer = 0;
+		[SerializeField] private float _timer = 0;
 
-        private void ClearMaze()
-        {
-            foreach (GameObject obj in _objects) Destroy(obj);
-            _objects = new List<GameObject>();
-        }
+		private void ClearMaze()
+		{
+			if (_objects.Count == 0) return;
+			float baseHeight = _objects[0].transform.localPosition.y;
+			const float displacement = 5f;
+			GameManager.GetMonoSystem<IAnimationMonoSystem>().RequestAnimation(
+				this,
+				_gateAudioClip.length,
+				(float t) => {
+					foreach (GameObject obj in _objects)
+					{
+						Vector3 p = obj.transform.localPosition;
+						obj.transform.localPosition = new Vector3(p.x, baseHeight - (t*t) * displacement, p.z);
+					}
+				},
+				() => {
+					foreach (GameObject obj in _objects) Destroy(obj);
+					_objects = new List<GameObject>();
+				}
+			);
+		}
 
-        private Vector2 GetEndIndex(Maze maze)
-        {
-            Vector2 maxIndex = Vector2.zero;
+		private Vector2 GetEndIndex(Maze maze)
+		{
+			Vector2 maxIndex = Vector2.zero;
 
-            int rows = maze.width;
-            int cols = maze.height;
+			int rows = maze.width;
+			int cols = maze.height;
 
-            for (int i = 1; i < rows - 1; i++)
-            {
-                for (int j = 1; j < cols - 1; j++)
-                {
-                    if (maze[i, j] == 1)
-                    {
-                        if (i > maxIndex.x || (i == maxIndex.x && j > maxIndex.y))
-                        {
-                            maxIndex = new Vector2(i, j);
-                        }
-                    }
-                }
-            }
+			for (int i = 1; i < rows - 1; i++)
+			{
+				for (int j = 1; j < cols - 1; j++)
+				{
+					if (maze[i, j] == 1)
+					{
+						if (i > maxIndex.x || (i == maxIndex.x && j > maxIndex.y))
+						{
+							maxIndex = new Vector2(i, j);
+						}
+					}
+				}
+			}
 
-            return maxIndex;
-        }
+			return maxIndex;
+		}
 
-        private void GenerateMaze()
-        {
-            if (_maze == null)
-            {
-                Debug.LogWarning("Trying to instantiate a maze without a Maze object being generated. Try Calling StartPuzzle instead!");
-                return;
-            }
+		private void GenerateMaze()
+		{
+			if (_maze == null)
+			{
+				Debug.LogWarning("Trying to instantiate a maze without a Maze object being generated. Try Calling StartPuzzle instead!");
+				return;
+			}
 
-            ClearMaze();
+			ClearMaze();
 
-            Vector2 endIndex = GetEndIndex(_maze);
+			Vector2 endIndex = GetEndIndex(_maze);
 
-            for (int y = 0; y < _maze.height; y++)
-            {
-                for (int x = 0; x < _maze.width; x++)
-                {
-                    GameObject floor = Instantiate(_floorPrefab);
-                    floor.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
-                    floor.transform.parent = _mazeHolder.transform;
-                    _objects.Add(floor);
+			for (int y = 0; y < _maze.height; y++)
+			{
+				for (int x = 0; x < _maze.width; x++)
+				{
+					GameObject floor = Instantiate(_floorPrefab);
+					floor.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
+					floor.transform.parent = _mazeHolder.transform;
 
-                    floor.transform.GetChild(2).gameObject.SetActive(Random.Range(0, 100) % 2 == 1);
+					floor.transform.GetChild(2).gameObject.SetActive(Random.Range(0, 100) % 2 == 1);
 
-                    if (_maze[x, y] == 1)
-                    {
-                        floor.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.white;
+					if (_maze[x, y] == 1)
+					{
+						floor.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.white;
 
-                        if (x == endIndex.x && y == endIndex.y)
-                        {
-                            GameObject key = Instantiate(_keyPrefab);
-                            key.transform.position = transform.position + new Vector3(y * _scaleZ, 1f, x * _scaleX);
-                            key.transform.parent = _mazeHolder.transform;
-                            _objects.Add(key);
+						if (x == endIndex.x && y == endIndex.y)
+						{
+							GameObject key = Instantiate(_keyPrefab);
+							key.transform.position = transform.position + new Vector3(y * _scaleZ, 1f, x * _scaleX);
+							key.transform.parent = _mazeHolder.transform;
+							_objects.Add(key);
 
-                            floor.transform.GetChild(4).gameObject.SetActive(true);
-                        } else if (x == 1 && y == 0)
-                        {
-                            floor.transform.GetChild(3).gameObject.SetActive(true);
-                        }
-
+							floor.transform.GetChild(4).gameObject.SetActive(true);
+						} else if (x == 1 && y == 0)
+						{
+							floor.transform.GetChild(3).gameObject.SetActive(true);
+						}
 
 
-                        continue;
-                    }
 
-                    floor.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.clear;
-                    floor.transform.GetChild(2).gameObject.SetActive(false); // Turns off light for this cell.
+						continue;
+					}
 
-                    GameObject wall = Instantiate(_wallPrefab);
-                    wall.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
-                    wall.transform.parent = _mazeHolder.transform;
-                    _objects.Add(wall);
-                }
-            }
-        }
+					floor.transform.GetChild(1).GetComponent<MeshRenderer>().material.color = Color.clear;
+					floor.transform.GetChild(2).gameObject.SetActive(false); // Turns off light for this cell.
 
-        private void OnUpdate()
-        {
-            _timer += Time.deltaTime;
-        }
+					GameObject wall = Instantiate(_wallPrefab);
+					wall.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
+					wall.transform.parent = _mazeHolder.transform;
+					_objects.Add(wall);
+				}
+			}
+		}
 
-        private void OpenGateStep(float t, Transform gate, float start, float end)
-        {
-            gate.localPosition = new Vector3(gate.localPosition.x, gate.localPosition.y, Mathf.Lerp(start, end, t));
-        }
+		private void OnUpdate()
+		{
+			_timer += Time.deltaTime;
+		}
 
-        private void OpenGate()
-        {
-            float start = _gate.localPosition.z;
-            float end = _gateLimits.y;
-            _gateAudioSrc.PlayOneShot(_gateAudioClip);
-            GameManager.GetMonoSystem<IAnimationMonoSystem>().RequestAnimation(
-                this,
-                _gateAudioClip.length,
-                (float t) => OpenGateStep(
-                    t,
-                    _gate,
-                    start,
-                    end
-                ),
-                () => StartTimer()
-            );
-        }
+		private void OpenGateStep(float t, Transform gate, float start, float end)
+		{
+			gate.localPosition = new Vector3(gate.localPosition.x, gate.localPosition.y, Mathf.Lerp(start, end, t));
+		}
 
-        private void StartTimer()
-        {
-            _timer = 0;
-            _isTimerRunning = true;
-        }
+		private void OpenGate()
+		{
+			float start = _gate.localPosition.z;
+			float end = _gateLimits.y;
+			_gateAudioSrc.PlayOneShot(_gateAudioClip);
+			GameManager.GetMonoSystem<IAnimationMonoSystem>().RequestAnimation(
+				this,
+				_gateAudioClip.length,
+				(float t) => OpenGateStep(
+					t,
+					_gate,
+					start,
+					end
+				),
+				() => StartTimer()
+			);
+		}
 
-        private void StopTimer()
-        {
-            _isTimerRunning = false;
-        }
+		private void StartTimer()
+		{
+			_timer = 0;
+			_isTimerRunning = true;
+		}
 
-        public void Begin()
-        {
-            if (_isStarted) return;
-           _isStarted = true;
-            OpenGate();
-        }
+		private void StopTimer()
+		{
+			_isTimerRunning = false;
+		}
 
-        public void End()
-        {
-            _isStarted = false;
-            StopTimer();
-            ClearMaze();
-            for (int y = 0; y < _maze.height; y++)
-            {
-                for (int x = 0; x < _maze.width; x++)
-                {
-                    if ((x != 0 && y != 0) && (x == 0 || y == 0 || x == _maze.width - 1 || y == _maze.height - 1))
-                    {
-                        GameObject wall= Instantiate(_wallPrefab);
-                        wall.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
-                        wall.transform.parent = _mazeHolder.transform;
-                        _objects.Add(wall);
-                    }
-                    else
-                    {
-                        GameObject floor = Instantiate(_floorPrefab);
-                        floor.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
-                        floor.transform.parent = _mazeHolder.transform;
-                        floor.transform.GetChild(2).gameObject.SetActive(Random.Range(0, 100) % 2 == 1);
-                        _objects.Add(floor);
-                    }
-                }
-            }
+		public void Begin()
+		{
+			if (_isStarted) return;
+		   _isStarted = true;
+			OpenGate();
+		}
 
-            GameManager.GetMonoSystem<IPuzzleMonoSystem>().FinishPuzzle(this, PuzzleType.Maze);
-        }
+		public void End()
+		{
+			_isStarted = false;
+			StopTimer();
+			ClearMaze();
+			for (int y = 0; y < _maze.height; y++)
+			{
+				for (int x = 0; x < _maze.width; x++)
+				{
+					if ((x != 0 && y != 0) && (x == 0 || y == 0 || x == _maze.width - 1 || y == _maze.height - 1))
+					{
+						GameObject wall= Instantiate(_wallPrefab);
+						wall.transform.position = transform.position + new Vector3(y * _scaleZ, 0, x * _scaleX);
+						wall.transform.parent = _mazeHolder.transform;
+						_objects.Add(wall);
+					}
+				}
+			}
 
-        public override void StartPuzzle()
-        {
-            _maze = MazeGenerator.Generate(_mazeWidth, _mazeHeight);
-            GenerateMaze();
-        }
+			GameManager.GetMonoSystem<IPuzzleMonoSystem>().FinishPuzzle(this, PuzzleType.Maze);
+		}
 
-        private void Awake()
-        {
-            _objects = new List<GameObject>();
+		public override void StartPuzzle()
+		{
+			_maze = MazeGenerator.Generate(_mazeWidth, _mazeHeight);
+			GenerateMaze();
+		}
 
-            _mazeHolder = new GameObject("Maze Holder");
-            _mazeHolder.transform.parent = transform;
+		private void Awake()
+		{
+			_objects = new List<GameObject>();
 
-            _scaleX = _wallPrefab.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.x;
-            _scaleZ = _wallPrefab.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.z;
+			_mazeHolder = new GameObject("Maze Holder");
+			_mazeHolder.transform.parent = transform;
 
-            StartPuzzle();
-        }
+			_scaleX = _wallPrefab.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.x;
+			_scaleZ = _wallPrefab.transform.GetChild(0).GetComponent<MeshRenderer>().bounds.size.z;
 
-        private void Update()
-        {
-            if (_isStarted && _isTimerRunning) OnUpdate();
-        }
-    }
+			StartPuzzle();
+		}
+
+		private void Update()
+		{
+			if (_isStarted && _isTimerRunning) OnUpdate();
+		}
+	}
 }
